2b034ea84f86c15e9435f8dcd8c4a9662f2abad1
Yautrose <86356629+Yautrose@users.noreply.github.com>
2022-04-25 20:37:21 +0300

    Set slider and view

--- RGB slider/ViewController.swift
+++ RGB slider/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var rgbView: UIImageView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        rgbView.backgroundColor = UIColor(red: 130/255, green: 115/255, blue: 151/255, alpha: 1)
+        rgbView.layer.cornerRadius = 10
+        
+    
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        redLabel.text = String(Int(redSlider.value))
+        greenLabel.text = String(Int(greenSlider.value))
+        blueLabel.text = String(Int(blueSlider.value))
+        
+        redLabel.textColor = .red
+        greenLabel.textColor = .green
+        blueLabel.textColor = .blue
+        
+        
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func redSliderAction() {
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value)/255, green: CGFloat(greenSlider.value)/255, blue: CGFloat(blueSlider.value)/255, alpha: 1)
+        
+        redLabel.text = String(Int(redSlider.value))
+    }
+    @IBAction func greenSliderAction() {
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value)/255, green: CGFloat(greenSlider.value)/255, blue: CGFloat(blueSlider.value)/255, alpha: 1)
+        
+        greenLabel.text = String(Int(greenSlider.value))
 
+    }
+    @IBAction func blueSliderAction() {
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value)/255, green: CGFloat(greenSlider.value)/255, blue: CGFloat(blueSlider.value)/255, alpha: 1)
+        
+        blueLabel.text = String(Int(blueSlider.value))
+    }
 }
 
